2a14e2fd9426483d5057feff92f6e8911912d8e2
semnavy70 <70761590+semnavy70@users.noreply.github.com>
2023-02-05 20:55:28 +0700

    no message

--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -62,7 +62,7 @@
                 <p>{{ __('voyager.login.signin_below') }}</p>
 
                 <form action="{{ route('voyager.login') }}" method="POST">
-                    {{ csrf_field() }}
+                    {!! csrf_field() !!}
                     <div class="form-group form-group-default" id="emailGroup">
                         <label>{{ __('voyager.generic.email') }}</label>
                         <div class="controls">
