2b83cb53946989cf337977ffbf8465cd43aae489
YUTHA <yuthakao015@gmail.com>
2023-08-14 21:30:40 +0700

    Fix news card style

--- resources/js/Pages/News/Index.vue
+++ resources/js/Pages/News/Index.vue
@@ -19,24 +19,26 @@
             <div class="news-section mt-5">
                 <div class="row gy-4">
                     <div class="col-12 col-sm-12 col-md-6 col-lg-4" v-for="post in paginate.data">
-                        <Link :href="route('news.detail' , post.id)">
-                            <div class="bg-white p-3 shadow">
-                                <img class="w-100" :src="getImage(post.image)" alt="">
-                                <p class="text-green fs-18 mb-1">{{ post.title }}</p>
-                                <div class="small-hr my-2"/>
-                                <p class="mt-2 fs-14">
-                                    <i class="fa-solid fa-user me-1"></i>
-                                    <span>{{ post.by }}</span>
-                                    <span> | {{ post.kh_created_at }}</span>
-                                </p>
-                                <p class="fs-16">{{ post.excerpt }}</p>
-                                <div class="text-end">
-                                    <Link :href="route('news.detail' , post.id)" class="text-green">
-                                        អានបន្ត<i class="fa-solid fa-angles-right"></i>
-                                    </Link>
+                        <div class="news-card">
+                            <Link :href="route('news.detail' , post.id)">
+                                <div class="bg-white p-3 shadow">
+                                    <img class="w-100" :src="getImage(post.image)" alt="">
+                                    <p class="text-green fs-18 my-2 text-truncate-2">{{ post.title }}</p>
+                                    <div class="small-hr my-2"/>
+                                    <p class="mt-2 fs-14">
+                                        <i class="fa-solid fa-user me-1"></i>
+                                        <span>{{ post.by }}</span>
+                                        <span> | {{ post.kh_created_at }}</span>
+                                    </p>
+                                    <p class="fs-16 text-truncate-3">{{ post.excerpt }}</p>
+                                    <div class="text-end">
+                                        <Link :href="route('news.detail' , post.id)" class="text-green">
+                                            អានបន្ត<i class="fa-solid fa-angles-right"></i>
+                                        </Link>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
