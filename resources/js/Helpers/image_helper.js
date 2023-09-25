680a33c0c6c14c6c822e96dac057806b57376c46
Yoem Yem <yyoem22@gmail.com>
2023-09-25 09:37:07 +0700

    No message

--- resources/js/Helpers/image_helper.js
+++ resources/js/Helpers/image_helper.js
@@ -1,24 +1,24 @@
 export default {
     methods: {
         getImage: function (path) {
-            if(path.includes("http")){
-                return  path;
+            if (path.includes("http")) {
+                return path;
             }
             let url = "https://storage.googleapis.com/indo-pacific/";
             return url + path;
         },
         getHomeImage: function (path) {
-            if(path.includes("http")){
-                return  path;
+            if (path.includes("http")) {
+                return path;
             }
             let url = "https://storage.googleapis.com/indo-pacific/";
             return url + path;
         },
         getMarketImage: function (path) {
-            if(!path) {
-                return  "/assets/img/no-picture.png";
+            if (!path) {
+                return "/assets/img/no-picture.png";
             }
-          return "https://storage.googleapis.com/amis_market/" + path;
+            return "https://storage.googleapis.com/amis_market/" + path;
         },
     }
 }
